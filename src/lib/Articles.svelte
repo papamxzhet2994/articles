a4af7c093510e703d8817b1ebff1404a07d4eab9
papamxzhet2994 <pvova4370@gmail.com>
2023-06-02 16:26:41 +0300

    another arrangement of cards

--- src/lib/Articles.svelte
+++ src/lib/Articles.svelte
@@ -22,52 +22,55 @@
 <div class="container mx-auto">
     <h1 class="text-3xl font-bold mb-4">Статьи</h1>
     {#if articles.length > 0}
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {#each articles as article}
-                <div class="border rounded-lg overflow-hidden shadow-lg">
-                    <div class="p-6">
-                        {#if article.urlToImage}
-                            <img class="w-full object-cover h-48 rounded-xl" src="{article.urlToImage}" alt="{article.title}">
-                        {:else}
-                            <div class="placeholder"></div>
-                        {/if}
-                        <h2 class="text-2xl font-bold mb-2">{article.title}</h2>
-                        <p class="text-gray-700">{article.description}</p>
-                        <a class="text-blue-500 hover:text-blue-700" href="{article.url}" target="_blank" rel="noopener">Читать далее</a>
+        <div class="cards-container">
+            <div class="cards">
+                {#each articles as article}
+                    <div class="card">
+                        <div class="border rounded-lg overflow-hidden shadow-lg w-64">
+                            <div class="p-6">
+                                {#if article.urlToImage}
+                                    <img class="w-full object-cover h-48 rounded-xl" src="{article.urlToImage}" alt="{article.title}">
+                                {:else}
+                                    <div class="placeholder"></div>
+                                {/if}
+                                <h2 class="text-2xl font-bold mb-2">{article.title}</h2>
+                                <p class="text-gray-700">{article.description}</p>
+                                <a class="text-blue-500 hover:text-blue-700" href="{article.url}" target="_blank" rel="noopener">Читать далее</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            {/each}
+                {/each}
+            </div>
         </div>
     {:else}
         <p class="text-gray-700">Загрузка статей...</p>
     {/if}
 </div>
 
+
 <style>
     .container {
         padding: 1rem;
-        max-width: 900px;
+        max-width: 500px;
         margin: 0 auto;
     }
 
-    .grid {
+    .cards {
         display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 0px;
     }
 
-    .grid-cols-1 {
-        grid-template-columns: repeat(1, minmax(0, 1fr));
+    .cards-container {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        grid-gap: 1rem;
+        justify-items: center;
     }
 
-    .grid-cols-2 {
-        grid-template-columns: repeat(2, minmax(0, 1fr));
-    }
 
-    .grid-cols-3 {
-        grid-template-columns: repeat(3, minmax(0, 1fr));
-    }
-
-    .gap-4 {
-        gap: 1rem;
+    .card {
+        margin: 0.5rem;
     }
 
     .border {
@@ -86,6 +89,7 @@
         background-color: #fff;
         box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
         color: #1a1a1a;
+        margin-bottom: 0;
     }
 
     img {
